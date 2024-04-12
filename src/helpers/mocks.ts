51d59612176d5e9d8b1668e21eea6f666db27b09
Bruno Rocha <bruno.rocha.dev@outlook.com>
2024-04-11 22:16:14 -0300

    Fix color tags

--- src/helpers/mocks.ts
+++ src/helpers/mocks.ts
@@ -1,32 +1,32 @@
 export const productsList = [
   {
-    title: "CAMISETA TREFOIL URBANA",
-    description:
-      "Perfeita para o dia a dia, a Camiseta Trefoil Urbana oferece conforto e estilo. Feita com algodão de alta qualidade, esta peça é ideal para qualquer ocasião, com um Trefoil discretamente posicionado. O algodão é sustentável, seguindo os padrões do Better Cotton.",
-    price: 160.0,
-    colors: ["#fff", "#007bff"],
-    sizes: ["P", "M", "G"],
-    imgUrl:
-      "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3a52745a85fc4fc4a2aa303c70dee61d_9366/Camiseta_Trefoil_Essentials_Cinza_IR9692_21_model.jpg",
-    id: 2,
-  },
-  {
     title: "CAMISETA CLASSIC TREFOIL",
     description:
       "A Camiseta Classic Trefoil redefine o conforto com um toque de classe. O icônico logo Trefoil acrescenta uma assinatura única, enquanto o tecido de algodão suave promove um uso agradável durante todo o dia. Participe da moda sustentável com o algodão Better Cotton.",
     price: 145.99,
-    colors: ["#fff", "#f00", "#008000"],
+    colors: ["#fff", "#000", "#8B0000"],
     sizes: ["P", "M", "G", "GG", "XG"],
     imgUrl:
       "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ce724fd7a0a04a498e14afd000e31835_9366/Camiseta_Adicolor_Classics_Trefoil_Branco_IM4494_21_model.jpg",
     id: 3,
   },
   {
+    title: "CAMISETA TREFOIL URBANA",
+    description:
+      "Perfeita para o dia a dia, a Camiseta Trefoil Urbana oferece conforto e estilo. Feita com algodão de alta qualidade, esta peça é ideal para qualquer ocasião, com um Trefoil discretamente posicionado. O algodão é sustentável, seguindo os padrões do Better Cotton.",
+    price: 160.0,
+    colors: ["#B8B8B8", "#fff", "#000", "#603E3E"],
+    sizes: ["P", "M", "G"],
+    imgUrl:
+      "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3a52745a85fc4fc4a2aa303c70dee61d_9366/Camiseta_Trefoil_Essentials_Cinza_IR9692_21_model.jpg",
+    id: 2,
+  },
+  {
     title: "CAMISETA TREFOIL CASUAL",
     description:
       "Com um design clean e confortável, a Camiseta Trefoil Casual é a escolha perfeita para o dia a dia. Fabricada com fibras de algodão sustentáveis, oferece um toque suave e uma postura consciente em relação ao meio ambiente.",
     price: 170.25,
-    colors: ["#fff", "#8B4513"],
+    colors: ["#fff", "#000"],
     sizes: ["M", "G", "GG"],
     imgUrl:
       "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/7d7d0535ce1942418d2c2d053edc6f7a_9366/Camiseta_Trefoil_Essentials_Branco_IR9691_21_model.jpg",
@@ -37,7 +37,7 @@ export const productsList = [
     description:
       "A Camiseta Street Trefoil traz o espírito urbano para o seu guarda-roupa. Seu design contemporâneo, junto ao emblemático logo Trefoil, oferece estilo e identidade. Feita para quem valoriza conforto e sustentabilidade.",
     price: 155.0,
-    colors: ["#5A3C3F", "#000"],
+    colors: ["#603E3E", "#000", "#fff", "#B8B8B8"],
     sizes: ["P", "M", "G"],
     imgUrl:
       "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/6fe28022ac7d490fab2a83d9d68daa45_9366/Camiseta_Adicolor_Classics_3-Stripes_Marrom_IM2077_21_model.jpg",
@@ -48,7 +48,7 @@ export const productsList = [
     description:
       "Ideal para os atletas de plantão, a Camiseta Trefoil Sport combina funcionalidade e estilo. Com tecnologia que absorve a umidade, esta camiseta te mantém seco e confortável durante os treinos, enquanto promove a sustentabilidade.",
     price: 180.5,
-    colors: ["#FD4450", "#0000FF"],
+    colors: ["#8B0000", "#fff"],
     sizes: ["P", "M", "GG"],
     imgUrl:
       "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/9c96e74d9d9841f1aa99d434c8344b53_9366/Camiseta_Manchester_United_Essentials_Trefoil_Vermelho_IK8705_21_model.jpg",
@@ -70,7 +70,7 @@ export const productsList = [
     description:
       "A Camiseta Trefoil Heritage presta homenagem às raízes clássicas da adidas, combinando um estilo vintage com a modernidade dos tecidos sustentáveis. Uma peça que traz história e conforto juntos.",
     price: 175.0,
-    colors: ["#8A2BE2", "#fff"],
+    colors: ["#fff", "#000"],
     sizes: ["P", "M", "G", "GG"],
     imgUrl:
       "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/be7760dd1bee4a2a916aaf6a012ee461_9366/Camiseta_Estampada_adidas_Rekive_Branco_HR8591_21_model.jpg",
@@ -81,7 +81,7 @@ export const productsList = [
     description:
       "Com foco na sustentabilidade, a Camiseta Trefoil Eco é feita 100% de materiais reciclados. Oferecendo não apenas estilo e conforto, mas também uma escolha consciente para o planeta.",
     price: 190.0,
-    colors: ["#fff", "#228B22"],
+    colors: ["#fff", "#000"],
     sizes: ["P", "M", "G"],
     imgUrl:
       "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/759f8a91204346b2a6c5afd000db4783_9366/Camiseta_Treino_Manga_Curta_Logo_Branco_IM4373_21_model.jpg",
@@ -92,7 +92,7 @@ export const productsList = [
     description:
       "A Camiseta Trefoil Premium eleva o nível de qualidade e design. Com detalhes refinados e tecido de algodão superior, esta camiseta é a definição de luxo casual. Engaje-se com o movimento Better Cotton, vestindo-se com propósito.",
     price: 200.0,
-    colors: ["#000", "#FFA500"],
+    colors: ["#000", "#fff"],
     sizes: ["P", "M", "G", "GG", "XG"],
     imgUrl:
       "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/fed78b4be09348919d5d66ae9bd94dc4_9366/Camiseta_Trefoil_Essentials_Preto_IR9690_21_model.jpg",
